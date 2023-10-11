9b7034b6d76ec54d112b16574ce0b0b99b024967
tsham11 <shamugiatamar22@gmail.com>
2023-10-11 11:59:46 +0400

    added script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+//this script is for toggle
+var buttons = document.getElementsByClassName("button");
+var arr = [...buttons];
+
+arr.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    element.style.opacity = "1";
+
+    arr
+      .filter(function (item) {
+        return item != element;
+      })
+      .forEach((item) => {
+        item.style.opacity = "0";
+      });
+  });
+});
